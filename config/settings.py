44a45b21a95d51c70aab9c1ca4e17a27dbc6f2da
kuroma36 <yarohi3636563n@gmail.com>
2026-09-22 20:12:56 +0900

    refactor: アプリケーションからfoodlogおよびfoodscanを完全に削除

--- config/settings.py
+++ config/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'udon',
     'travel.apps.TravelConfig',
     'pomodoro.apps.PomodoroConfig',
-    'foodscan.apps.FoodscanConfig',
-    'foodlog.apps.FoodlogConfig',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -25,8 +25,6 @@ urlpatterns = [
     path('udon/', include('udon.urls')),
     path('travel/', include('travel.urls')),
     path('pomodoro/', include('pomodoro.urls')),
-    path('foodscan/', include('foodscan.urls')),
-    path('foodlog/', include('foodlog.urls')),
     path('', RedirectView.as_view(url='/udon/', permanent=False)),
     # 旧APIリクエストの互換用リダイレクト
     re_path(r'^api/(?P<path>.*)$', RedirectView.as_view(url='/udon/api/%(path)s', permanent=False)),
